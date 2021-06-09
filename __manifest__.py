317728af0057424eb886d2109adf168fb9207d97
Your Name <you@example.com>
2021-06-08 20:58:21 -0400

    Reporte orden de lavandería

--- __manifest__.py
+++ __manifest__.py
@@ -26,6 +26,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/report.xml',
         'security/ir.model.access.csv'
     ],
     # only loaded in demonstration mode
